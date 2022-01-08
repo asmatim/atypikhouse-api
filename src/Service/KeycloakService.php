0d28797f48831c0f647633499e2c8d218dbdb6ac
Asma TIMZIT <atimzit@gmail.com>
2022-01-08 15:55:37 +0100

    Implement update userRole and Password in keyckloak

--- src/Service/KeycloakService.php
+++ src/Service/KeycloakService.php
@@ -3,6 +3,7 @@
 namespace App\Service;
 
 use App\Entity\User;
+use Psr\Log\LoggerInterface;
 use RuntimeException;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,14 +17,16 @@ class KeycloakService
     private $keycloakClientSecret;
     private $keycloakServerUrl;
     private $keycloakRealm;
+    private $logger;
 
-    public function __construct(HttpClientInterface $client, $keycloakClientId, $keycloakClientSecret, $keycloakServerUrl, $keycloakRealm)
+    public function __construct(HttpClientInterface $client, $keycloakClientId, $keycloakClientSecret, $keycloakServerUrl, $keycloakRealm, LoggerInterface $logger)
     {
         $this->httpClient = $client;
         $this->keycloakClientId = $keycloakClientId;
         $this->keycloakClientSecret = $keycloakClientSecret;
         $this->keycloakServerUrl = $keycloakServerUrl;
         $this->keycloakRealm = $keycloakRealm;
+        $this->logger = $logger;
     }
 
     public function createUser(User $user)
@@ -38,13 +41,38 @@ class KeycloakService
             ],
             'json' => $this->getUserData($user),
         ]);
-        if ($response->getStatusCode() == Response::HTTP_OK) {
-            $contents = $response->toArray();
-            return $contents["access_token"];
+
+        if ($response->getStatusCode() == Response::HTTP_CREATED) {
+            return $this->getUserExternalIdFromHeader($response);
         }
+
         if ($response->getStatusCode() !== Response::HTTP_CREATED) {
-            //var_dump($response->getContent());
             throw new RuntimeException("Error couldn't create User.");
+        }
+    }
+
+    public function updateUser(User $user)
+    {
+        $token = $this->getClientToken();
+        $bearerToken = "Bearer " . $token;
+
+        $this->logger->debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>update");
+        $this->logger->debug($this->getUpdateUserUrl($user->getExternalId()));
+
+        $response = $this->httpClient->request('PUT', $this->getUpdateUserUrl($user->getExternalId()), [
+            'headers' => [
+                'Content-Type' => 'application/json ',
+                'Authorization' => $bearerToken,
+            ],
+            'json' => $this->getUserData($user),
+        ]);
+
+        if ($response->getStatusCode() == Response::HTTP_NO_CONTENT) {
+            return;
+        }
+
+        if ($response->getStatusCode() !== Response::HTTP_NO_CONTENT) {
+            throw new RuntimeException("Error couldn't update User.");
         }
     }
 
@@ -84,7 +112,7 @@ class KeycloakService
 
     private function getUserData(User $user)
     {
-        return [
+        $userData = [
             "email" => $user->getEmail(),
             "username" => $user->getUsername(),
             "firstName" => $user->getFirstName(),
@@ -92,25 +120,57 @@ class KeycloakService
             "enabled" => true,
             "attributes" => [
                 "roles" => json_encode($user->getRoles())
-            ],
-            "credentials" => [
+            ]
+        ];
+
+        if (!empty($user->getPlainPassword())) {
+            $userData["credentials"] = [
                 [
                     "type" => "password",
                     "value" => $user->getPlainPassword(),
                     "temporary" => false,
                 ]
-            ]
-        ];
+            ];
+        }
+
+        $this->logger->debug(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>payload keycloak user data");
+        $this->logger->debug(var_export($userData, true));
+
+        return $userData;
+    }
+
+    private function getUserExternalIdFromHeader($response)
+    {
+        $responseHeaders = $response->getHeaders();
+        $userLocationHeader = $responseHeaders["location"];
+
+        if (count($userLocationHeader) == 0) {
+            throw new RuntimeException("Error couldn't create User.");
+        }
+
+        // get the first element of the array and remove it from array
+        $userLocation = array_shift($userLocationHeader);
+
+        $splitedUserLocation = explode("/", $userLocation);
+
+        $userId = end($splitedUserLocation);
+
+        return $userId;
     }
 
     private function getTokenUrl()
     {
         return $this->keycloakServerUrl . 'realms/' . $this->keycloakRealm . '/protocol/openid-connect/token';
     }
-    
+
     private function getCreateUserUrl()
     {
         return $this->keycloakServerUrl . 'admin/realms/' . $this->keycloakRealm . '/users';
+    }
+
+    private function getUpdateUserUrl($userExternalId)
+    {
+        return $this->keycloakServerUrl . 'admin/realms/' . $this->keycloakRealm . '/users/' . $userExternalId;
     }
 
     private function getIntrospectUrl()
